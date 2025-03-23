52fa49f42314defe680287919a178415a77540d4
sharad <138323667+sharad-77@users.noreply.github.com>
2025-03-23 12:09:27 +0530

    Home Page Completed

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -15,23 +15,23 @@ function Home() {
   return (
     <section className="bg-gray-20 h-full w-full font-custom">
       {/* Main Section */}
-      <div className="flex flex-col justify-center items-center h-full w-full pt-20 box-pattern gradient-container md:px-10 lg:px-20 xl:px-40">
-        <div className="mini-Heading text-center md:text-left">The Future of Learning is Here</div>
+      <div className="flex flex-col justify-center items-center h-full w-full pt-20 box-pattern gradient-container md:px-10 lg:px-10 xl:px-20">
+        <div className="mini-Heading text-center md:text-left ">The Future of Learning is Here</div>
 
-        <div className="bg-gradient-to-r from-purple-600 to-indigo-600 text-transparent bg-clip-text text-[72px] max-w-4xl text-center font-bold leading-none py-4 md:text-[96px] lg:text-[120px] xl:text-[144px]">
+        <div className="bg-gradient-to-r from-purple-600 to-indigo-600 text-transparent bg-clip-text max-w-4xl lg:max-w-3xl text-center font-bold leading-none py-4 text-5xl md:text-6xl lg:text-7xl xl:text-[72px]">
           Unlock Your Potential with SkillBridge
         </div>
 
-        <div className="text-[#4b5563] max-w-3xl text-center text-xl leading-normal py-5 md:text-left md:px-10 lg:px-20 xl:px-40">
+        <div className="text-[#4b5563] max-w-3xl md:max-w-lg lg:max-w-2xl text-center leading-normal md:py-2 lg:py-5 text-md font-semibold md:text-md lg:text-xl">
           Connect, collaborate, and grow with a community of learners and
           professionals. Transform your skills into success.
         </div>
 
-        <div className="flex gap-4 py-2 items-center md:justify-center md:gap-8 lg:gap-12 xl:gap-16">
+        <div className="flex gap-4 py-8 items-center md:justify-center md:gap-8 lg:gap-12 xl:gap-16">
           <Button
             Variant="primary"
             size="large"
-            className="text-xl items-center gap-2 sparkle-effect text-black border-2 border-blue-700 hover:bg-white md:px-10 lg:px-12 xl:px-16"
+            className="text-md max-w-[10rem] max-h-[3rem] md:max-w-[14rem] md:text-lg items-center gap-2 sparkle-effect text-black border-2 border-black hover:bg-white md:px-6 py-6"
           >
             Get Started Free
             <MoveRight />
@@ -40,13 +40,13 @@ function Home() {
           <Button
             Variant="secondry"
             size="medium"
-            className="backdrop-blur-[90%] border-black border-2 md:px-6 lg:px-8 xl:px-10"
+            className="backdrop-blur-[90%] border-black border-2 text-md lg:text-lg md:px-6 lg:px-8 xl:px-10"
           >
             Watch Demo
           </Button>
         </div>
 
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 place-items-center gap-8 py-10 h-full max-w-4xl md:max-w-6xl lg:max-w-full xl:max-w-full">
+        <div className="grid grid-cols-2 md:grid-cols-4 place-items-center gap-8 py-10 h-full max-w-4xl md:max-w-6xl lg:max-w-full xl:max-w-full">
           <MiniCard>
             <div>10k+</div>
             <div>Active Users</div>
@@ -68,7 +68,7 @@ function Home() {
           </MiniCard>
         </div>
 
-        <div className="flex flex-col justify-center items-center bottom-10 left-1/2 transform -translate-x-1/2 animate-bounce ml-20 py-10 md:ml-0 md:px-10 lg:px-20 xl:px-40">
+        <div className="flex flex-col justify-center items-center bottom-10 left-1/2 transform -translate-x-1/2 animate-bounce ml-20 ">
           <p className="text-lg text-[var(--secoundry-color)]">Scroll Down</p>
           <ChevronDown className="text-[var(--secoundry-color)]" />
         </div>
@@ -78,13 +78,13 @@ function Home() {
       <div className="flex flex-col justify-center items-center py-10 md:px-10 lg:px-20 xl:px-40">
         <div className="flex flex-col justify-center items-center max-w-7xl">
           <div className="mini-Heading ">Features</div>
-          <div className="text-5xl font-bold py-3">Everything you need to succeed</div>
-          <div className="text-[#4b5563] max-w-2xl p-2 text-center text-lg leading-normal md:text-left md:px-10 lg:px-20 xl:px-40">
+          <div className=" text-3xl md:text-4xl text-center lg:text-5xl font-bold py-3">Everything you need to succeed</div>
+          <div className="text-[#4b5563] max-w-[23rem] md:max-w-lg p-2 text-center text-md lg:text-lg leading-normal">
             SkillBridge provides all the tools and resources you need to enhance
             your skills and advance your career.
           </div>
 
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8 px-4 py-10 h-full w-full md:max-w-6xl lg:max-w-full xl:max-w-full">
+          <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-8 px-4 py-10 h-full w-full md:max-w-6xl lg:max-w-full xl:max-w-full">
             <FeaturesCard
               svg={<Users color="#9333ea" size={38} />}
               title={"Project Collaboration"}
@@ -124,13 +124,13 @@ function Home() {
         </div>
       </div>
 
-      {/* Join Message */}
+      {/* Join Now Section */}
       <div className="flex justify-center items-center w-full h-full gradient-primary md:px-10 lg:px-20 xl:px-40">
-        <div className="flex flex-col justify-center items-center max-w-4xl h-[20rem] w-full glass-dark m-20 rounded-3xl text-center md:text-left">
-          <div className="text-white text-4xl font-semibold">
+        <div className="flex flex-col justify-center items-center max-w-4xl h-[20rem] w-full glass-dark m-20 rounded-3xl text-center md:text-left mx-10">
+          <div className="text-white text-4xl font-semibold ">
             Ready to Start Your Journey?
           </div>
-          <div className="text-white text-xl max-w-2xl py-8">
+          <div className="text-white md:max-w-md text-lg xl:text-xl text-center py-8">
             Join thousands of learners who are already transforming their
             careers with SkillBridge.
           </div>
@@ -141,16 +141,16 @@ function Home() {
       </div>
 
       {/* Testimonial */}
-      <div className="flex items-center justify-center pt-10 md:px-10 lg:px-20 xl:px-40">
+      <div className="flex items-center justify-center pt-10 ">
         <div className="flex flex-col justify-center items-center max-w-7xl">
           <p className="mini-Heading">Testimonials</p>
-          <p className="text-5xl font-semibold pt-2">What Our Users Say</p>
-          <p className="max-w-xl text-center text-gray-600 py-3 md:text-left md:px-10 lg:px-20 xl:px-40">
+          <div className="text-4xl md:text-5xl lg:text-5xl xl:text-6xl text-center font-bold pt-2">What Our Users Say</div>
+          <div className="max-w-lg md:max-w-md text-center text-gray-600 py-3 ">
             Hear from students and professionals who have transformed their
             careers with SkillBridge.
-          </p>
+          </div>
 
-          <div className="grid grid-cols-3 gap-8 py-8 px-4 h-full w-full md:max-w-6xl lg:max-w-full xl:max-w-full">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 place-items-center  gap-8 py-8 px-4 h-full w-full md:max-w-6xl lg:max-w-full xl:max-w-full">
             <div className="h-full w-full border-t-4 border-[#9333ea]">
               <PostComponent>
                 <div>
