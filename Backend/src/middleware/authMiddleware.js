44c4b95c244c93f3df2a00d044bd06e92f55a6a6
Chavda <chavdasharad77@gmail.com>
2025-05-19 23:42:31 +0530

    x

---
+++ Backend/src/middleware/authMiddleware.js
@@ -0,0 +1,21 @@
+import jwt form "jsonwebtoken";
+
+const jwtsecret = process.env.JWT_SECRET;
+
+const authMiddleware = (req, res, next) => {
+    const authHeader = req.headers.authorization;
+    if (!authHeader || !authHeader.startsWith('Bearer ')) {
+        return res.status(401).json({ message: 'No token provided' });
+    }
+
+    const token = authHeader.split(' ')[1];
+    try {
+        const decoded = jwt.verify(token,jwtsecret);
+        req.user = decoded;
+        next();
+    } catch (err) {
+        return res.status(401).json({ message: 'Invalid token' });
+    }
+};
+
+export default authMiddleware;
